2834f2c30f0ab9232668a6b2c8569372e9bb47fb
illmilo <illfqm@gmail.com>
2025-06-30 18:11:17 +0300

    fix: vercel not displaying images from media folder

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ echo "BUILD START"
 python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic --noinput --clear
-python3 createsu.py
+python3 manage.py collectstatic --noinput
+
+mkdir -p staticfiles_build/media/
+cp -r media/* staticfiles_build/media/
+
 echo "BUILD END"
